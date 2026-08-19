018861b16ba3e3cacc48b06797c16417020ae233
fabawi <fabawi@users.noreply.github.com>
2026-08-19 16:30:12 +0000

    deploy: 4e96d0c80c3bfe93b9689a7f877bfdd98bc446e4

---
+++ img/screenshot_site.sh
@@ -0,0 +1,90 @@
+##################################### PNG ####################################
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/codemirror-lang-latex/
+convert temp.png -crop 1320x1080+300+0 +repage codemirror-lang-latex.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/codemirror-lang-bib/
+convert temp.png -crop 1320x1080+300+0 +repage codemirror-lang-bib.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/wasm-latex-tools/
+convert temp.png -crop 1320x1080+300+0 +repage wasm-latex-tools.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/texlyre-busytex/
+convert temp.png -crop 1320x1080+300+0 +repage texlyre-busytex.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/codemirror-latex-visual/
+convert temp.png -crop 1320x1080+300+0 +repage codemirror-latex-visual.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/vector-pdf-converter/
+convert temp.png -crop 1320x1080+300+0 +repage vector-pdf-converter.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/indicant/
+convert temp.png -crop 1320x1080+300+0 +repage indicant.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/filepizza-client/
+convert temp.png -crop 1320x1080+300+0 +repage filepizza-client.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/texlyre-templates/
+convert temp.png -crop 1320x1080+300+0 +repage texlyre-templates.png
+rm temp.png
+
+google-chrome --headless --no-sandbox --hide-scrollbars \
+  --window-size=1920,1080 \
+  --incognito \
+  --screenshot=temp.png \
+  --virtual-time-budget=10000 \
+  https://texlyre.org/chelys-recipes/
+convert temp.png -crop 1320x1080+300+0 +repage chelys-recipes.png
+rm temp.png
